101d41439d07827ed8b742739d1b9908b96127ef
Fabian <20464654j@iespoblenou.org>
2017-05-23 10:21:51 +0200

    Mostramos en un textview el valor cuando seleccionamos el valor con la seekbar y el resultado final en un toast

--- app/src/main/java/com/example/fabian/seekbartest/MainActivity.java
+++ app/src/main/java/com/example/fabian/seekbartest/MainActivity.java
@@ -8,15 +8,44 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar sb1;
+    private TextView tvSb1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sb1 = (SeekBar) findViewById( R.id.seekBar1 );
+        tvSb1 = (TextView) findViewById( R.id.tv_seek_bar_1 );
+
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progressChanged = 0;
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+                tvSb1.setText( String.valueOf( progressChanged ) );
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText( MainActivity.this, "seek bar progress:"+progressChanged,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
